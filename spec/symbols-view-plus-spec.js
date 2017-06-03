c34470d62eb5e88f4a04680a5541916b8c18e7c6
Aidi Stan <aidistan@live.com>
2017-06-04 02:09:28 +0800

    Simplify symbols-view-plus-spec.js

--- spec/symbols-view-plus-spec.js
+++ spec/symbols-view-plus-spec.js
@@ -8,11 +8,9 @@ import temp from 'temp';
 import {it, conditionPromise} from './async-spec-helpers';
 
 describe('SymbolsViewPlus', () => {
-
-  let [symbolsView, activationPromise, directory] = [];
+  let directory;
 
   const getWorkspaceView = () => atom.views.getView(atom.workspace);
-  const getEditorView = () => atom.views.getView(atom.workspace.getActiveTextEditor());
 
   beforeEach(async () => {
     jasmine.unspy(global, 'setTimeout');
@@ -25,8 +23,7 @@ describe('SymbolsViewPlus', () => {
     directory = atom.project.getDirectories()[1];
     fs.copySync(path.join(__dirname, 'fixtures', 'js'), atom.project.getPaths()[1]);
 
-    activationPromise = atom.packages.activatePackage('symbols-view-plus');
-    jasmine.attachToDOM(getWorkspaceView());
+    await atom.packages.activatePackage('symbols-view-plus');
   });
 
   it('generates project symbols', async () => {
@@ -37,18 +34,5 @@ describe('SymbolsViewPlus', () => {
     expect(tags.split('\n').length).toBeGreaterThan(13);
 
     fs.removeSync(directory.resolve('.tags'));
-  });
-
-  it('toggles a right panel', async () => {
-    await atom.workspace.open(directory.resolve('sample.js'));
-    atom.commands.dispatch(getEditorView(), 'symbols-view-plus:toggle-file-symbols');
-    await activationPromise;
-
-    symbolsView = atom.workspace.getRightPanels()[0].item;
-    await conditionPromise(() => symbolsView.element.querySelectorAll('li').length > 0);
-
-    expect(symbolsView.selectListView.refs.loadingMessage).toBeUndefined();
-    expect(document.body.contains(symbolsView.element)).toBe(true);
-    expect(symbolsView.element.querySelectorAll('li').length).toBe(2);
   });
 });
